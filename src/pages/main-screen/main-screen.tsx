f4b4b9535f9637085ce86a60dff6402a99c20b7b
SneG95 <Dima6233@yandex.ru>
2023-07-21 13:49:05 +0300

    Добавит и встроит компонент Header

--- src/pages/main-screen/main-screen.tsx
+++ src/pages/main-screen/main-screen.tsx
@@ -1,4 +1,4 @@
-import Logo from '../../components/logo/logo';
+import Header from '../../components/header/header';
 import {CardsList} from '../../components/cards-list/cards-list';
 import {Offer} from '../../types/offer';
 import {Helmet} from 'react-helmet-async';
@@ -10,30 +10,7 @@ type MainPageProps = {
 function MainScreen({offers}: MainPageProps): JSX.Element {
   return(
     <div className="page page--gray page--main">
-      <header className="header">
-        <div className="container">
-          <div className="header__wrapper">
-            <Logo />
-            <nav className="header__nav">
-              <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <a className="header__nav-link header__nav-link--profile" href="#">
-                    <div className="header__avatar-wrapper user__avatar-wrapper">
-                    </div>
-                    <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                    <span className="header__favorite-count">3</span>
-                  </a>
-                </li>
-                <li className="header__nav-item">
-                  <a className="header__nav-link" href="#">
-                    <span className="header__signout">Sign out</span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </header>
+      <Header />
 
       <main className="page__main page__main--index">
         <Helmet>
