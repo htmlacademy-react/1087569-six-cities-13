7fdb1348c53ed8608ad862e7f7d7a20dd756cda2
SneG95 <Dima6233@yandex.ru>
2023-08-20 15:20:58 +0300

    Настроит маршрутизацию при авторизации

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,6 +1,13 @@
 import Logo from '../logo/logo';
+import { useAppSelector, useAppDispatch } from '../../hooks';
+import { Link } from 'react-router-dom';
+import { logoutAction } from '../../store/api-actions';
+import { AuthorizationStatus, AppRoute } from '../../const';
 
 function Header(): JSX.Element {
+  const authorizationStatus = useAppSelector((state) => state.authorizationStatus);
+  const dispatch = useAppDispatch();
+
   return (
     <header className="header">
       <div className="container">
@@ -8,19 +15,35 @@ function Header(): JSX.Element {
           <Logo />
           <nav className="header__nav">
             <ul className="header__nav-list">
-              <li className="header__nav-item user">
-                <a className="header__nav-link header__nav-link--profile" href="#">
-                  <div className="header__avatar-wrapper user__avatar-wrapper">
-                  </div>
-                  <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                  <span className="header__favorite-count">3</span>
-                </a>
-              </li>
-              <li className="header__nav-item">
-                <a className="header__nav-link" href="#">
-                  <span className="header__signout">Sign out</span>
-                </a>
-              </li>
+              {authorizationStatus === AuthorizationStatus.Auth ?
+                <>
+                  <li className="header__nav-item user">
+                    <Link className="header__nav-link header__nav-link--profile" to={AppRoute.Favorites}>
+                      <div className="header__avatar-wrapper user__avatar-wrapper">
+                      </div>
+                      <span className="header__user-name user__name">Dima6233@yandex.ru</span>
+                      <span className="header__favorite-count">3</span>
+                    </Link>
+                  </li>
+                  <li className="header__nav-item">
+                    <Link
+                      onClick={(evt) => {
+                        evt.preventDefault();
+                        dispatch(logoutAction());
+                      }}
+                      className="header__nav-link"
+                      to={AppRoute.Root}
+                    >
+                      <span className="header__signout">Sign out</span>
+                    </Link>
+                  </li>
+                </>
+                :
+                <li className="header__nav-item">
+                  <Link className="header__nav-link" to={AppRoute.Login}>
+                    <span className="header__signout">Sign in</span>
+                  </Link>
+                </li>}
             </ul>
           </nav>
         </div>
